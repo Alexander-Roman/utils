70892c44a3841b55ef287c029af3049b8b85cccf
”Alexander <alexander.kitanin@gmail.com>
2021-03-08 18:54:22 +0300

    Build configured, StringUtils implementation added, StringUtils test added

---
+++ src/main/java/com/epam/utils/StringUtils.java
@@ -0,0 +1,16 @@
+package com.epam.utils;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.math.BigDecimal;
+
+public class StringUtils {
+
+    public boolean isPositiveNumber(String str) throws NumberFormatException {
+        if (!NumberUtils.isCreatable(str)) {
+            return false;
+        }
+        BigDecimal bigDecimal = NumberUtils.createBigDecimal(str);
+        return bigDecimal.compareTo(BigDecimal.ZERO) > NumberUtils.INTEGER_ZERO;
+    }
+}
